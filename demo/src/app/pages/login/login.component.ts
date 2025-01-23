16e413e4efb7b64cac71ddf798d6c6b4c258c5ed
Adel LATIBI <alatibi@campus-eni.fr>
2025-01-23 13:32:36 +0100

    update demo Formulaire

--- demo/src/app/pages/login/login.component.ts
+++ demo/src/app/pages/login/login.component.ts
@@ -17,9 +17,10 @@ export class LoginComponent {
   constructor(
     private authService: AuthService
   ) {
+    const StrongPasswordRegx: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d)(?=.*[*!@$%^&:;<>,]).{8,}$/;
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required,Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required,Validators.pattern(StrongPasswordRegx)]),
     })
   }
 
@@ -33,6 +34,29 @@ export class LoginComponent {
     // reinit
     this.loginForm.reset();
     this.isSubmitted = false;
+  }
+
+  checkErrors(controlName: string) {
+    if(!this.isSubmitted) return null;
+    const control  = this.loginForm.get(controlName);
+    if(!control || !control.errors) return null;
+
+    return Object.keys(control.errors).map(key => {
+      switch (key) {
+        case 'required':
+          return 'This field is required.';
+        case 'minlength':
+          return `Minimum length is ${control.errors?.[key].requiredLength}.`;
+        case 'email':
+          return 'Invalid email format.';
+        case 'pattern':
+          return 'Please put a strong Password, and must be at least 8 characters and contains At least one digit, At least one lowercase character, At least one uppercase character, At least one special character.';
+        default:
+          return 'Invalid input.';
+      }
+    });
+
+    return [];
   }
 
 }
